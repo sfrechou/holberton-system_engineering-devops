f10ad6507a25813c0bc68c557df51e3e0469525f
Soledad Frechou <solefrechou@hotmail.com>
2021-04-06 22:39:31 +0000

    Add

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,22 @@
+# Install nginx
+class{ 'nginx':
+  package_source => 'nginx',
+  package_name   => 'nginx',
+}
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt',
+}
+# root / with a GET request using curl, it must return a page that contains the string Holberton School
+file { 'index.html':
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School',
+}
+
+# The redirection must be a “301 Moved Permanently”
+file_line { '/redirect_me':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;'
+  line   => 'rewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
