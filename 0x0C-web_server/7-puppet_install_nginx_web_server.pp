f758fd40b4225088b8900179b0ed67f40dbc6e69
Soledad Frechou <solefrechou@hotmail.com>
2021-04-06 23:01:23 +0000

    Add

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,13 +2,12 @@
 package { 'nginx':
   ensure   => installed,
 }
-# root / with a GET request using curl, it must return a page that contains the string Holberton School
+
 file { 'index.html':
   path    => '/var/www/html/index.html',
   content => 'Holberton School',
 }
 
-# The redirection must be a “301 Moved Permanently”
 file_line { 'redirect_me':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
