6593b0aa9e81288946534fe3c0dd5aca5fe99562
Soledad Frechou <solefrechou@hotmail.com>
2021-04-12 23:18:55 +0000

    Add

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,14 @@
+# Install nginx
+package { 'nginx':
+  ensure   => installed,
+  provider => 'apt',
+  name     => 'nginx',
+}
+
+location /etc/nginx/sites-available/default {
+  add_header X-Served-B $HOSTNAME ;      
+}
+
+service { 'nginx':
+  ensure  => 'running',
+}
