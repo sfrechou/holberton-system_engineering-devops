4f295fd2e06e5bd35d5705534292f45684befa73
Soledad Frechou <solefrechou@hotmail.com>
2021-04-12 23:58:37 +0000

    Add

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,7 +1,6 @@
-# Install nginx
+# Add header
 exec {'Barney':
   command  => 'sudo apt-get -y update; sudo apt-get -y install nginx;
-               sudo ufw allow 'Nginx HTTP';
                sudo sed -i "46i\ add_header X-Served-By $HOSTNAME ;" /etc/nginx/sites-available/default;
                sudo service nginx restart',
   provider => 'shell',
