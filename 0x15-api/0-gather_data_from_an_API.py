77632c3a333c984f29be8afc427dfcb1257f3638
Soledad Frechou <solefrechou@hotmail.com>
2021-05-03 18:45:36 +0000

    Add 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ url = 'https://jsonplaceholder.typicode.com/users?id='
 u_id = argv[1]
 m = requests.get(url + u_id)
 n = m.json()
-EMPLOYEE_NAME = n[0]['name']
+EMPLOYEE_NAME = n[0].get('name')
 
 o = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + u_id)
 p = o.json()
@@ -25,4 +25,4 @@ print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
 for content in range(len(p)):
     for key, value in p[content].items():
         if key == 'completed' and value is True:
-            print("\t{}".format(p[content]['title']))
+            print("\t{}".format(p[content].get('title')))
