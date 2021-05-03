6b278c85c439f611225f5586c33eba85e75b63e5
Soledad Frechou <solefrechou@hotmail.com>
2021-05-03 19:01:07 +0000

    Add 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,30 +3,27 @@
 import requests
 from sys import argv
 
-
-url = 'https://jsonplaceholder.typicode.com/users?id='
-u_id = argv[1]
-m = requests.get(url + u_id)
-n = m.json()
-EMPLOYEE_NAME = n[0].get('name')
-
-o = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + u_id)
-p = o.json()
-TOTAL_NUMBER_OF_TASKS = len(p)
-NUMBER_OF_DONE_TASKS = 0
-for content in range(len(p)):
-    for key, value in p[content].items():
-        if key == 'completed':
-            if value is True:
-                NUMBER_OF_DONE_TASKS += 1
-
-print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
-                                                      NUMBER_OF_DONE_TASKS,
-                                                      TOTAL_NUMBER_OF_TASKS))
-for content in range(len(p)):
-    for key, value in p[content].items():
-        if key == 'completed' and value is True:
-            print("\t {}".format(p[content].get('title')))
-
 if __name__ == "__main__":
-    pass
+    url = 'https://jsonplaceholder.typicode.com/users?id='
+    u = argv[1]
+    m = requests.get(url + u)
+    n = m.json()
+    EMPLOYEE_NAME = n[0].get('name')
+
+    o = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + u)
+    p = o.json()
+    TOTAL_TASKS = len(p)
+    NUMBER_OF_DONE_TASKS = 0
+    for content in range(len(p)):
+        for key, value in p[content].items():
+            if key == 'completed':
+                if value is True:
+                    NUMBER_OF_DONE_TASKS += 1
+
+    print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                          NUMBER_OF_DONE_TASKS,
+                                                          TOTAL_TASKS))
+    for content in range(len(p)):
+        for key, value in p[content].items():
+            if key == 'completed' and value is True:
+                print("\t {}".format(p[content].get('title')))
