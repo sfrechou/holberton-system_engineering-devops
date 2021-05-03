8a79957f6c71b8859b5c982c4d58e53049b8f210
Soledad Frechou <solefrechou@hotmail.com>
2021-05-03 18:43:38 +0000

    Add 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Script that returns information about given employee's TODO list progress"""
+import requests
+from sys import argv
+
+url = 'https://jsonplaceholder.typicode.com/users?id='
+u_id = argv[1]
+m = requests.get(url + u_id)
+n = m.json()
+EMPLOYEE_NAME = n[0]['name']
+
+o = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + u_id)
+p = o.json()
+TOTAL_NUMBER_OF_TASKS = len(p)
+NUMBER_OF_DONE_TASKS = 0
+for content in range(len(p)):
+    for key, value in p[content].items():
+        if key == 'completed':
+            if value is True:
+                NUMBER_OF_DONE_TASKS += 1
+
+print("Employee {} is done with tasks({}/{}):".format(EMPLOYEE_NAME,
+                                                      NUMBER_OF_DONE_TASKS,
+                                                      TOTAL_NUMBER_OF_TASKS))
+for content in range(len(p)):
+    for key, value in p[content].items():
+        if key == 'completed' and value is True:
+            print("\t{}".format(p[content]['title']))
