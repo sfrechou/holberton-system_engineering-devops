7cc6c8602a0130077aa70f124b321b127cc7e201
sfrechou <solefrechou@hotmail.com>
2021-05-18 23:05:53 +0000

    Add 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers"""
+    url = "https://www.reddit.com/r/" + subreddit + "/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        print(response.json()['data'].get('subscribers'))
