32728412b5198664d134a7c3e5fb007a44d7c78a
sfrechou <solefrechou@hotmail.com>
2021-05-18 23:14:36 +0000

    Add 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers"""
     url = "https://www.reddit.com/r/" + subreddit + "/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         return response.json()['data'].get('subscribers')
