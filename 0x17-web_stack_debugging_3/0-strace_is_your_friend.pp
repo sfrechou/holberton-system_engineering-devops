891b9255904ae4647a48aa8d05dfc5f9c605cb87
sfrechou <solefrechou@hotmail.com>
2021-05-19 19:06:17 +0000

    Add 0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Find out why Apache is returning a 500 error
 exec { 'fixit':
-  command  => 'sudo sed -i "80i\ define('WP_DEBUG', true);" /var/www/html/wp-config.php';
+  command  => 'sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php';
   provider => 'shell',
 }
